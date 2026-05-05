ce923b58d42763bab2c7bc2c5260479e634ba2f1
notashwin0 <vjashwin2006@gmail.com>
2026-05-05 22:54:29 +0530

    Added base code for the To-Do List

---
+++ old-projects/02-todo-list/script.js
@@ -0,0 +1,26 @@
+const myButton = document.getElementById("mainButton")
+const mainList = document.getElementById("orderList")
+
+count = 0
+
+function addTask(){
+    count++
+    newTask = document.createElement('li')
+    newTask.textContent = `${count} Custom Task`
+    mainList.appendChild(newTask)
+
+    newTask.addEventListener('click', function(){
+        this.remove()
+    })
+}
+
+myButton.addEventListener('click', addTask)
+
+// todo list 
+// 1. Add a text field to add custom tasks
+// 2. Each task must have an Edit and Delete button, possibly as icons
+// 3. (?) Do checked tasks get deleted
+// 4. (?) When editing a task, do I use the Pop Up or edit through the text itself (if that is possible)
+// 5. Use CSS
+// 6. Research Local Storage 
+// 7. (?) Research if this uses forms
