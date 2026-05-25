de9c6354f2d5ab5426b81697de0008992fb415ac
notashwin0 <vjashwin2006@gmail.com>
2026-05-25 19:03:50 +0530

    Added the task adder function with two buttons

--- old-projects/02-todo-list/script.js
+++ old-projects/02-todo-list/script.js
@@ -1,15 +1,36 @@
-const task_input = document.getElementById("task-input")
-const task_list = document.getElementById("task-list")
+const task_input = document.getElementById("task-input") 
+const task_list = document.getElementById("task-list") // List of Tasks
 
 task_input.addEventListener("keydown", function(event) {
     if (event.key === "Enter"){
-        const text = task_input.value;
-        
-        const taskToAppend = document.createElement("p");
-        taskToAppend.textContent = text
+        const text = task_input.value; // grabs the task input
+        if (text !== "") { 
+            const taskToAppend = document.createElement("div");
+            taskToAppend.classList.add("task-item") // Gives the DIV a class of task-item
 
-        task_list.appendChild(taskToAppend)
+            const taskText = document.createElement("span") // inline container
+            taskText.textContent = text
+            
+            // EDIT BUTTON
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit"
+            editButton.classList.add ("edit-Button")
 
-        input.value = "" 
+            // DELETE BUTTON
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete"
+            deleteButton.classList.add ("delete-Button")
+
+            // appends the text and two buttons to the task DIV
+            taskToAppend.appendChild(taskText)
+            taskToAppend.appendChild(editButton)
+            taskToAppend.appendChild(deleteButton)
+
+            // appends the task div to the list of tasks
+            task_list.appendChild(taskToAppend)
+
+            task_input.value = ""
+
+        }
     }
 });
