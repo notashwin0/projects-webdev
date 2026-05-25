d335504ed57be73c15f08eb614309441ca731416
notashwin0 <vjashwin2006@gmail.com>
2026-05-25 20:17:19 +0530

    Checkbox Functionality

--- old-projects/02-todo-list/script.js
+++ old-projects/02-todo-list/script.js
@@ -7,6 +7,10 @@ task_input.addEventListener("keydown", function(event) {
         if (text !== "") { 
             const taskToAppend = document.createElement("div");
             taskToAppend.classList.add("task-item") // Gives the DIV a class of task-item
+            
+            const checkbox = document.createElement("input")
+            checkbox.type = "checkbox"
+            checkbox.classList.add("task-checkbox")
 
             const taskText = document.createElement("span") // inline container
             taskText.textContent = text
@@ -22,6 +26,7 @@ task_input.addEventListener("keydown", function(event) {
             deleteButton.classList.add ("delete-Button")
 
             // appends the text and two buttons to the task DIV
+            taskToAppend.appendChild(checkbox)
             taskToAppend.appendChild(taskText)
             taskToAppend.appendChild(editButton)
             taskToAppend.appendChild(deleteButton)
@@ -34,3 +39,35 @@ task_input.addEventListener("keydown", function(event) {
         }
     }
 });
+
+task_list.addEventListener("click", function(event){
+    const target = event.target 
+
+    // checkbox logic
+
+    if (target.classList.contains("task-checkbox")) {
+        const taskItem = target.closest(".task-item");
+        const textSpan = taskItem.querySelector("span");
+        textSpan.classList.toggle("completed");
+    }
+
+    // delete button logic
+
+    if (target.classList.contains("delete-Button")){
+        const taskItem = target.closest(".task-item")
+        taskItem.remove()
+    }
+
+    // edit button logic
+
+    if (target.classList.contains("edit-Button")){ // checks if clicked element has this class
+        const taskItem = target.closest(".task-item"); // finds the parent div with this class
+        const textSpan = taskItem.querySelector("span") // finds the span in the task item
+        const currentText = textSpan.textContent; // gets the current task text
+        const updatedText = prompt("Edit the task :", currentText) // opens a prompt with the text already previewed
+
+        if (updatedText !== null && updatedText.trim() !== ""){ // check if user didnt cancel and new text isnt empty or white space
+            textSpan.textContent = updatedText;
+        }
+    }
+})
